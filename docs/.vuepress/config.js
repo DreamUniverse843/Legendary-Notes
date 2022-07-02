c6d820d9f8f43f82b161e8b428dcc13c389d9426
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2022-07-02 18:29:56 +0800

    Change N plus set expression to Times

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     themeConfig :{
         repo : 'DreamCraft-Tech/DreamCraft-Docs',
-        docsBranch : 'master',
+        docsBranch : 'main',
         docsDir : 'docs',
         editLinks : true,
         locales :{
