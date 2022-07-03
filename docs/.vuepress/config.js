ef11781b4847285b73f351466ef099be36b73481
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2022-07-03 16:23:52 +0800

    History-Optional Book 1 Lesson 5,6,7

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,6 +56,9 @@ module.exports = {
                 'lesson2',
                 'lesson3',
                 'lesson4',
+                'lesson5',
+                'lesson6',
+                'lesson7',
             ],
             '/':[
                 '',
